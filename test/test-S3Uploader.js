181bc1417705a84cab58f4090f66808151061cc9
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-01-15 10:19:40 +0800

    S3Uploader supports uploading Buffer type file.

--- test/test-S3Uploader.js
+++ test/test-S3Uploader.js
@@ -1,10 +1,23 @@
 var S3Uploader = require('../lib/S3Uploader');
+var fs = require('fs');
 
 var uploader = new S3Uploader();
-var params = {
-  File: 'Path to your uploaded file', /* Required */
-  Bucket: 'Your destination bucket name', /* Required */
-  Key: 'Your destination object key', /* Requred */
+// Test for File as string
+var params0 = {
+  File: 'Path to your first uploaded file', /* Required */
+  Bucket: 'Your first destination bucket name', /* Required */
+  Key: 'Your first destination object key', /* Requred */
+  options: {
+    ACL: 'public-read',
+    ContentType: 'image/jpeg',
+    StorageClass: 'STANDARD'
+  }
+};
+// Test for File as Buffer
+var params1 = {
+  File: new Buffer(0), /* Required */
+  Bucket: 'Your second destination bucket name', /* Required */
+  Key: 'Your second destination object key', /* Requred */
   options: {
     ACL: 'public-read',
     ContentType: 'image/jpeg',
@@ -22,5 +35,14 @@ uploader.on('success', function(data) {
   console.log('Uploading progress: ' + (loaded / total * 100) + '%, ' + loaded + '/' + total + ' bytes');
 });
 
-uploader.send(params);
+uploader.send(params0);
+
+var buf = [];
+var readStream = fs.createReadStream('Path to your second uploaded file');
+readStream.on('data', function(chunk) {
+  buf.push(chunk);
+}).on('end', function() {
+  params1['File'] = Buffer.concat(buf);
+  uploader.send(params1);
+});
 
