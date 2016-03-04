469cd78b0e74566dfd93343716e081ce6c6b7ef7
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-03-04 04:19:36 +0000

    S3Remover.js supports deleting objects at once.

--- test/test-S3Remover.js
+++ test/test-S3Remover.js
@@ -1,9 +1,19 @@
 var S3Remover = require('../lib/S3Remover');
 
 var remover = new S3Remover();
-var params = {
+// test for deleting an object.
+var params0 = {
   Bucket: 'Your target bucket name', /* Required */
-  Key: 'Your target object name', /* Requred */
+  Key: 'Your target object name' /* Requred */
+};
+// test for deleting multiple objects.
+var params1 = {
+  Bucket: 'Your target bucket name', /* Required */
+  Key: [ /* Requred */
+    'Your first target object name',
+    'Your second target object name',
+    'Your third target object name'
+  ]
 };
 
 remover.on('success', function(data) {
@@ -12,5 +22,6 @@ remover.on('success', function(data) {
   console.log('Erro occuring while deleting: ', err);
 });
 
-remover.remove(params);
+remover.remove(params0);
+remover.remove(params1);
 
